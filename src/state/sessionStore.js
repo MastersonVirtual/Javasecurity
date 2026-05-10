9ef3186c9cb23e8b5a023e5193c8f300a93676b1
MastersonVirtual <mastersoncomercialytecnica@gmail.com>
2026-05-09 22:49:20 -0300

    Consolidate Sentinel Ops into single HTML

--- src/state/sessionStore.js
+++
@@ -1,9 +0,0 @@
-export const SESSION_KEY = 'sentinel-session';
-
-export function loadSession() {
-  return JSON.parse(localStorage.getItem(SESSION_KEY) || 'null');
-}
-
-export function saveSession(session) {
-  localStorage.setItem(SESSION_KEY, JSON.stringify(session));
-}
--- src/utils/format.js
+++
@@ -1,7 +0,0 @@
-export function cn(...classes) {
-  return classes.filter(Boolean).join(' ');
-}
-
-export function formatTime(value) {
-  return new Date(value).toLocaleTimeString('es-AR', { hour: '2-digit', minute: '2-digit' });
-}
